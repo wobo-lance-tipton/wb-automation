8027ed390a61be7e345c2360360ad01cc32e8613
wobolancetipton <wobo-lance-tipton@goblet.io>
2022-08-23 02:05:33 +0000

    test(goblet): goblet.config.js - add auto-commit

---
+++ goblet.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  recorder: {
+    locator: `-- 🍷 GOBLET`,
+  },
+  paths: {
+    repoRoot: __dirname,
+    workDir: 'goblet',
+    artifactsDir: 'artifacts',
+    reportsDir: 'artifacts/reports',
+    featuresDir: 'bdd/features',
+    supportDir: 'bdd/support',
+    world: 'world',
+    stepsDir: 'bdd/steps',
+    unitDir: 'unit',
+    waypointDir: 'waypoint',
+  },
+}
